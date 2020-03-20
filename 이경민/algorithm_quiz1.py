ad09d17ad764cad89640efbf247a7b20adcfe8f3
kyeongmin <ekmst1027@gmail.com>
2020-03-20 11:07:47 +0900

    Finish solving the algorithm_quiz by KyeongMin

---
+++ 이경민/algorithm_quiz1.py
@@ -0,0 +1,19 @@
+'''
+1.
+팩토리얼은 1부터 n까지 연속한 숫자의 곱이라 합니다.
+팩토리얼을 함수(factorial)로 구현하는데 재귀함수를 이용하여 구현하시오.
+
+<입력>
+print(factorial(10))
+
+<출력>
+3628800'''
+
+
+def factorial(num):
+    if num == 1:
+        return 1
+    return num * factorial(num-1)
+
+
+print(factorial(10))
